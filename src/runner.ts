5de97d8d68bb71415cbab45b25eca13a449e5fc3
Gopi Krishna <gopi.slaerdalblr@gmail.com>
2026-03-18 16:32:01 +0530

    Changed the project name from pw-cucumber-ts to RQI/LLP-Playwright modified package-lock.json, package.json, readme.md and generate-report.ts files

--- src/runner.ts
+++ src/runner.ts
@@ -10,9 +10,9 @@ import fs from "node:fs";
  */
 const RUN_CONFIG = {
   // Change these defaults whenever you want
-  instance: "maurya", // instance key from src/config/instances.json
+  instance: "qa-samurai", // instance key from src/config/instances.json
   //tags: "@test or @regression",
-  tags: "@test",
+  tags: "@tcts",
   parallel: 4,
 
   // NOTE:
@@ -22,7 +22,7 @@ const RUN_CONFIG = {
   //browser: "chromium" + "firefox" + "webkit", // chromium | firefox | webkit | "all" | "chromium+firefox+webkit"
   // browser: "all",
   browser: "chromium",
-  headless: true,
+  headless: false,
 
   // Toggle this to control dry run
   dryRun: false,
